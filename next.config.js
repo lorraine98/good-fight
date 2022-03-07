27deef76b67f5a0eac95c3c79ad8db15563fcb5e
naeunchan <eunchan0492@naver.com>
2022-03-07 23:51:01 +0900

    chore: npm to yarn으로 변경

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    webpack(config) {
-        config.resolve.modules.push(__dirname);
-        return config;
-    },
 };
 
 module.exports = nextConfig;
