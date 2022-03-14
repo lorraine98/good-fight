d83bcb7282fe83d9b5b494374b5577814eb9693b
naeunchan <eunchan0492@naver.com>
2022-03-14 13:45:16 +0900

    feature: header 뒤로가기, 현재 페이지, 알림 컴포넌트 생성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
     const router = useRouter();
 
     const title: Title = {
-        "/home": "홈",
+        "/": "홈",
         "/your-fights": "니쌈",
         "/my-fights": "내쌈",
         "/my-page": "마이 페이지",
@@ -16,7 +16,38 @@ const Header = () => {
 
     const currentRoute = title[router.pathname];
 
-    return <div>{currentRoute}</div>;
+    return (
+        <>
+            <div className="header">
+                <div className="wrapper">
+                    <div className="backbutton">뒤로가기</div>
+                    <div className="route">{currentRoute}</div>
+                </div>
+                <div className="wrapper">
+                    <div className="notification">알림</div>
+                </div>
+            </div>
+            <style jsx>{`
+                .header {
+                    display: flex;
+                    align-items: center;
+                    width: 100%;
+                    height: 50px;
+                    font-size: 1.2rem;
+                    font-weight: bold;
+                    justify-content: space-between;
+                }
+
+                .header .wrapper {
+                    display: flex;
+                }
+
+                .header .wrapper div {
+                    margin: 0 0.5rem;
+                }
+            `}</style>
+        </>
+    );
 };
 
 export default Header;
