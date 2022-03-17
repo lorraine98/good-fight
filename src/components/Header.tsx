af5f051a7eaa485ad63b2f918b1cdfa5d5b7494c
naeunchan <eunchan0492@naver.com>
2022-03-17 14:39:26 +0900

    chore: prettierrc 추가 및 navbar 카테고리 정렬

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,61 +5,61 @@ import NotificationsNone from "@mui/icons-material/NotificationsNone";
 import NotificationAdd from "@mui/icons-material/NotificationAdd";
 
 interface Title {
-    [key: string]: string;
+  [key: string]: string;
 }
 
 const Header = () => {
-    const router = useRouter();
-    const [notification, setNotification] = useState(false);
-    const [isRoot, setIsRoot] = useState(false);
+  const router = useRouter();
+  const [notification, setNotification] = useState(false);
+  const [isRoot, setIsRoot] = useState(false);
 
-    const title: Title = {
-        "/": "홈",
-        "/your-fights": "니쌈",
-        "/my-fights": "내쌈",
-        "/my-page": "마이 페이지",
-    };
+  const title: Title = {
+    "/": "홈",
+    "/your-fights": "니쌈",
+    "/my-fights": "내쌈",
+    "/my-page": "마이 페이지",
+  };
 
-    const currentRoute = title[router.pathname];
+  const currentRoute = title[router.pathname];
 
-    return (
-        <>
-            <div className="header">
-                <div className="wrapper">
-                    <div className="backbutton">{isRoot ? "" : <ArrowBack />}</div>
-                    <div className="route">{currentRoute}</div>
-                </div>
-                <div className="wrapper">
-                    <div className="notification">
-                        {notification ? <NotificationAdd /> : <NotificationsNone />}
-                    </div>
-                </div>
-            </div>
-            <style jsx>{`
-                .header {
-                    display: flex;
-                    margin: 0 auto;
-                    align-items: center;
-                    width: 30vw;
-                    height: 50px;
-                    font-size: 1rem;
-                    font-weight: bold;
-                    justify-content: space-between;
-                    border: 1px solid black;
-                    min-width: 500px;
-                }
+  return (
+    <>
+      <div className="header">
+        <div className="wrapper">
+          <div className="backbutton">{isRoot ? "" : <ArrowBack />}</div>
+          <div className="route">{currentRoute}</div>
+        </div>
+        <div className="wrapper">
+          <div className="notification">
+            {notification ? <NotificationAdd /> : <NotificationsNone />}
+          </div>
+        </div>
+      </div>
+      <style jsx>{`
+        .header {
+          display: flex;
+          margin: 0 auto;
+          align-items: center;
+          width: 30vw;
+          height: 50px;
+          font-size: 1rem;
+          font-weight: bold;
+          justify-content: space-between;
+          border: 1px solid black;
+          min-width: 500px;
+        }
 
-                .header .wrapper {
-                    display: flex;
-                    align-items: center;
-                }
+        .header .wrapper {
+          display: flex;
+          align-items: center;
+        }
 
-                .header .wrapper div {
-                    margin: 0 0.5rem;
-                }
-            `}</style>
-        </>
-    );
+        .header .wrapper div {
+          margin: 0 0.5rem;
+        }
+      `}</style>
+    </>
+  );
 };
 
 export default Header;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,77 +10,76 @@ import PersonIcon from "@mui/icons-material/Person";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 
 const NavBar = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <nav>
-            <Link href="/">
-                <a>
-                    {router.pathname === "/" ? (
-                        <HomeIcon fontSize="large" />
-                    ) : (
-                        <HomeOutlinedIcon fontSize="large" />
-                    )}
-                    <h3>홈</h3>
-                </a>
-            </Link>
-            <Link href="/your-fights">
-                <a>
-                    {router.pathname === "/your-fights" ? (
-                        <ThumbsUpDownIcon fontSize="large" />
-                    ) : (
-                        <ThumbsUpDownOutlinedIcon fontSize="large" />
-                    )}
-                    <h3>니쌈</h3>
-                </a>
-            </Link>
-            <div></div>
-            <Link href="/my-fights">
-                <a>
-                    {router.pathname === "/my-fights" ? (
-                        <LibraryBooksIcon fontSize="large" />
-                    ) : (
-                        <LibraryBooksOutlinedIcon fontSize="large" />
-                    )}
-                    <h3>내쌈</h3>
-                </a>
-            </Link>
-            <Link href="/my-page">
-                <a>
-                    {router.pathname === "/my-page" ? (
-                        <PersonIcon fontSize="large" />
-                    ) : (
-                        <PersonOutlineOutlinedIcon fontSize="large" />
-                    )}
-                    <h3>마이페이지</h3>
-                </a>
-            </Link>
-            <style jsx>{`
-                nav {
-                    position: fixed;
-                    bottom: 0;
-                    left: 0;
-                    right: 0;
-                    margin: 0 auto;
-                    display: flex;
-                    font-size: 15px;
-                    border: 1px solid black;
-                    width: 30vw;
-                    height: 60px;
-                    justify-content: space-evenly;
-                    align-items: center;
-                    min-width: 500px;
-                }
+  return (
+    <nav>
+      <Link href="/">
+        <a>
+          {router.pathname === "/" ? (
+            <HomeIcon fontSize="large" />
+          ) : (
+            <HomeOutlinedIcon fontSize="large" />
+          )}
+          <h3>홈</h3>
+        </a>
+      </Link>
+      <Link href="/your-fights">
+        <a>
+          {router.pathname === "/your-fights" ? (
+            <ThumbsUpDownIcon fontSize="large" />
+          ) : (
+            <ThumbsUpDownOutlinedIcon fontSize="large" />
+          )}
+          <h3>니쌈</h3>
+        </a>
+      </Link>
+      <Link href="/my-fights">
+        <a>
+          {router.pathname === "/my-fights" ? (
+            <LibraryBooksIcon fontSize="large" />
+          ) : (
+            <LibraryBooksOutlinedIcon fontSize="large" />
+          )}
+          <h3>내쌈</h3>
+        </a>
+      </Link>
+      <Link href="/my-page">
+        <a>
+          {router.pathname === "/my-page" ? (
+            <PersonIcon fontSize="large" />
+          ) : (
+            <PersonOutlineOutlinedIcon fontSize="large" />
+          )}
+          <h3>마이페이지</h3>
+        </a>
+      </Link>
+      <style jsx>{`
+        nav {
+          position: fixed;
+          bottom: 0;
+          left: 0;
+          right: 0;
+          margin: 0 auto;
+          display: flex;
+          font-size: 15px;
+          border: 1px solid black;
+          width: 30vw;
+          height: 60px;
+          justify-content: space-around;
+          align-items: center;
+          min-width: 500px;
+        }
 
-                nav a {
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    align-items: center;
-                }
-            `}</style>
-        </nav>
-    );
+        nav a {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
+    </nav>
+  );
 };
 
 export default NavBar;
