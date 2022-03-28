e2f3944c920fc78c5936b419c52e5cd7cf559763
Lorraine <limgytjd98@gmail.com>
2022-03-29 03:40:21 +0900

    Feat: add button to my fights

--- src/pages/my-fights/index.tsx
+++ src/pages/my-fights/index.tsx
@@ -1,5 +1,14 @@
-const index = () => {
-    return <div></div>;
+import { useRouter } from "next/router";
+import AddButton from "../../shared/components/add-button";
+
+const MyFights = () => {
+  const { push } = useRouter();
+
+  const handleClick = () => {
+    push("my-fights/add");
+  };
+
+  return <AddButton onClick={handleClick} />;
 };
 
-export default index;
+export default MyFights;
