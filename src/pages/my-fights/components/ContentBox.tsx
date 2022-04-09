2f2e93690e7a364cbf0111706453065b9281d66e
lorraine98 <hyosung0425@naver.com>
2022-04-09 11:01:58 +0900

    Style: style my fights contentBox

--- src/pages/my-fights/components/ContentBox.tsx
+++ src/pages/my-fights/components/ContentBox.tsx
@@ -1,5 +1,7 @@
+import { MoreVert } from "@mui/icons-material";
 import { useTheme } from "@mui/system";
 import MyFightsStatusIcon from "src/shared/components/MyFightsStatusIcon";
+import Tag from "./Tag";
 
 const ContentBox = () => {
   const theme = useTheme();
@@ -7,13 +9,49 @@ const ContentBox = () => {
   return (
     <>
       <div className="wrapper">
-        <MyFightsStatusIcon state="solved" size="large" />
-        설거지 안함 설거지 안함
+        <div className="upperInfo-wrapper">
+          <MyFightsStatusIcon state="solved" size="large" />
+          <div className="upperInfo">
+            <div className="upperInfo-tags">
+              <Tag
+                text="설거지"
+                type="keyword"
+                style={{ marginRight: "1rem" }}
+              />
+              <Tag text="아무개" type="target" />
+            </div>
+            <span>2022.02.08</span>
+          </div>
+        </div>
+        <MoreVert sx={{ position: "absolute", right: 12, top: 16 }} />
+        <div className="lowerInfo">
+          <h1 className="content">설거지를 제때 안함</h1>
+          <p>설거지를 제때 해야 벌레가 안생김</p>
+          <p>정해진 날짜에 맞춰 설거지 꼭 하자!</p>
+        </div>
       </div>
       <style jsx>{`
         .wrapper {
           background: ${theme.palette.white};
           border-radius: 0.375rem;
+          padding: 1rem 1rem 0.5rem 1rem;
+          position: relative;
+        }
+        .upperInfo-wrapper {
+          display: flex;
+          margin-bottom: 1rem;
+        }
+        .upperInfo {
+          margin: 0.8rem 1.6rem;
+        }
+        .upperInfo-tags {
+          margin-bottom: 1rem;
+        }
+        .lowerInfo > * {
+          margin-bottom: 0.8rem;
+        }
+        .content {
+          font-weight: 600;
         }
       `}</style>
     </>
