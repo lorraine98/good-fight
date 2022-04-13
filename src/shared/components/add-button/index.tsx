b152438a1f21ed216036b8f6efe6e4d50a586262
naeunchan <eunchan0492@naver.com>
2022-04-13 18:33:14 +0900

    refactor: add button 그림자 수정

--- src/shared/components/add-button/index.tsx
+++ src/shared/components/add-button/index.tsx
@@ -1,4 +1,4 @@
-import { AddCircle } from "@mui/icons-material";
+import { AddCircleRounded } from "@mui/icons-material";
 import { useTheme } from "@mui/system";
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
@@ -7,22 +7,35 @@ const AddButton = ({ onClick }: Props) => {
   const theme = useTheme();
 
   return (
-    <button onClick={onClick}>
-      <AddCircle
-        sx={{
-          color: theme.palette.custom.brown,
-          boxShadow:
-            "rgb(0 0 0 / 20%) 0px 3px 3px -2px, rgb(0 0 0 / 14%) 0px 3px 4px 0px, rgb(0 0 0 / 12%) 0px 1px 8px 0px",
-          fontSize: 100,
-          borderRadius: 50,
-          zIndex: 3,
-          cursor: "pointer",
-          position: "absolute",
-          bottom: 80,
-          right: 30,
-        }}
-      />
-    </button>
+    <>
+      <button onClick={onClick}>
+        <div className="background" />
+        <AddCircleRounded
+          sx={{
+            color: theme.palette.custom.brown,
+            fontSize: 80,
+            borderRadius: 50,
+            zIndex: 3,
+            cursor: "pointer",
+            position: "absolute",
+            bottom: 80,
+            right: 30,
+          }}
+        />
+      </button>
+      <style jsx>{`
+        .background {
+          position: absolute;
+          bottom: 87px;
+          right: 37px;
+          background-color: white;
+          width: 66px;
+          height: 66px;
+          border-radius: 50%;
+          filter: drop-shadow(0px 8px 3px rgb(0 0 0 / 40%));
+        }
+      `}</style>
+    </>
   );
 };
 
