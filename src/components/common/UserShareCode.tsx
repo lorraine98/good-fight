e5b7040944fa636b7596ca65d721a701625803de
lorraine98 <limgytjd98@gmail.com>
2022-07-10 00:36:50 +0900

    Add short uuid

--- src/components/common/UserShareCode.tsx
+++ src/components/common/UserShareCode.tsx
@@ -2,9 +2,11 @@ import Button from "src/shared/components/button";
 import logo from "src/shared/img/logo.png";
 import Image from "next/image";
 import { useTheme } from "@mui/system";
+import shortUUID from "short-uuid";
 
 const UserShareCode = () => {
   const theme = useTheme();
+  const uuid = shortUUID.generate();
 
   return (
     <>
@@ -13,8 +15,8 @@ const UserShareCode = () => {
       </div>
       <p className="title">공유 코드예요!</p>
       <p className="desc">아래 코드로 상대방과 연결할 수 있어요.</p>
-      <div className="code">s8dg9sd9f9</div>
-      <Button style={{ width: "216px" }}>복사하기</Button>
+      <span className="code">{uuid}</span>
+      <Button style={{ width: "268px" }}>복사하기</Button>
       <button className="know-code">상대방의 코드를 알고 있어요!</button>
       <style jsx>{`
         .img {
@@ -27,9 +29,11 @@ const UserShareCode = () => {
         }
         .code {
           border: 1px solid ${theme.palette.custom.gray};
+          min-width: 268px;
           border-radius: 8px;
-          padding: 18px 64px;
+          padding: 16px;
           margin: 24px 0 12px 0;
+          text-align: center;
         }
         .know-code {
           text-decoration: underline;
